81b7ed011827dca9c602f4a5109d573e431745a2
butterbroad <pyatochevskiy@gmail.com>
2020-11-09 18:11:49 +0300

    initial commit

---
+++ #src/js/script.js
@@ -0,0 +1,29 @@
+//register links
+const registerLinks = document.querySelectorAll('.get-link');
+
+if (registerLinks.length > 0) {
+  registerLinks.forEach(item => {
+    item.setAttribute('href', item.getAttribute('href') + window.location.search);
+  });
+}
+
+//modal 
+const modalTrigger = document.querySelector('.hero__info');
+
+modalTrigger.addEventListener('click', () => {
+  const modal = document.querySelector('.modal');
+  modal.classList.add('active');
+
+  const modalClose = document.querySelector('.modal-info__close');
+  modalClose.addEventListener('click', () => {
+    modal.classList.remove('active');
+  });
+
+});
+
+
+
+
+
+
+@@include('webpSupport.js')
---
+++ football-territory/js/script.js
@@ -0,0 +1,45 @@
+//register links
+const registerLinks = document.querySelectorAll('.get-link');
+
+if (registerLinks.length > 0) {
+  registerLinks.forEach(item => {
+    item.setAttribute('href', item.getAttribute('href') + window.location.search);
+  });
+}
+
+//modal 
+const modalTrigger = document.querySelector('.hero__info');
+
+modalTrigger.addEventListener('click', () => {
+  const modal = document.querySelector('.modal');
+  modal.classList.add('active');
+
+  const modalClose = document.querySelector('.modal-info__close');
+  modalClose.addEventListener('click', () => {
+    modal.classList.remove('active');
+  });
+
+});
+
+
+
+
+
+
+function testWebP(callback) {
+
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    } else {
+        document.querySelector('body').classList.add('no-webp');
+    }
+});
